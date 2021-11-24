f9b968ce177d1168f35b6563e5943fe653041798
Kulachenko Aleks <hitsugayatru@gmail.com>
2021-11-24 18:27:02 +0300

    Smooth camera zoom

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    [SerializeField] private Player _player;
+    [SerializeField] private PlayerJump _playerJump;
     [SerializeField] private Vector3 _offset;
     [SerializeField] private Vector3 _zoomOffset;
-    [SerializeField] private Player _player;
-    [SerializeField] private Vector3 _zoomRotation;
-    [SerializeField] private PlayerJump _playerJump;
+    [SerializeField] private float _unzoomedAngleX;
+    [SerializeField] private float _zoomedAngleX;
     [SerializeField] private float _followSpeed;
     [SerializeField] private float _zoomSpeed;
+    [SerializeField] private float _accelerationZoomCoefficient;
 
-    private float _lastSpeed;
     private Vector3 _lastOffset;
     private Quaternion _startQuaternion;
     private Quaternion _zoomQuaternion;
+    private bool _returnStartQuaternion = false;
     private IEnumerator _coroutine;
     private bool _coroutineIsActive = false;
 
     private void Awake()
     {
-        _startQuaternion = Quaternion.Euler(transform.position);
-        _zoomQuaternion = Quaternion.Euler(_zoomRotation);
+        _startQuaternion = Quaternion.Euler(new Vector3(_unzoomedAngleX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z));
+        _zoomQuaternion = Quaternion.Euler(new Vector3(_zoomedAngleX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z));
+
         _lastOffset = _offset;
-        _lastSpeed = _followSpeed;
     }
 
     private void Start()
@@ -46,7 +48,7 @@ public class Camera : MonoBehaviour
 
     private void Update()
     {
-        Follow(_player, _lastOffset, _lastSpeed);
+        Follow(_player, _lastOffset);
     }
 
     private Vector3 SetStartPosition(Player player, Vector3 offset)
@@ -54,58 +56,50 @@ public class Camera : MonoBehaviour
         return new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, player.transform.position.z + offset.z);
     }
 
-    private void Follow(Player player, Vector3 offset, float speed)
+    private void Follow(Player player, Vector3 offset)
     {
         Vector3 target = SetStartPosition(player, offset);
-        Vector3 currentPosition = Vector3.Lerp(transform.position, target, speed * Time.deltaTime);
+        Vector3 currentPosition = Vector3.Lerp(transform.position, target, _followSpeed * Time.deltaTime);
         transform.position = currentPosition;
     }
 
     private void OnSlicing()
     {
-        /*if (_coroutineIsActive == true)
-            StopActiveCoroutine();
-
-        _coroutine = ChangeRotation(_zoomQuaternion);
-        StartCoroutine(_coroutine);*/
-
-        if (_coroutineIsActive == true)
-            StopActiveCoroutine();
-
-        _lastSpeed = _zoomSpeed;
-        _lastOffset = _zoomOffset;
-
-        _coroutine = ChangeRotation(_zoomQuaternion);
-        StartCoroutine(_coroutine);
+        SetFollowTarget(_zoomOffset, _zoomQuaternion);
+        _returnStartQuaternion = true;
     }
 
     private void OnJumped(bool clockwiseRotation)
     {
-        /*if (_coroutineIsActive == true)
-            StopActiveCoroutine();
-
-        _lastSpeed = _followSpeed;
-        _lastOffset = _offset;
-
-        if(transform.rotation != _startQuaternion)
+        if(_returnStartQuaternion == true)
         {
-            _coroutine = ChangeRotation(_startQuaternion);
-            StartCoroutine(_coroutine);
-        }*/
+            _returnStartQuaternion = false;
+            SetFollowTarget(_offset, _startQuaternion);
+        }
     }
 
-    /*private void OnSliced()
+    private void SetFollowTarget(Vector3 offset, Quaternion quaternion)
     {
+        if (_coroutineIsActive == true)
+            StopActiveCoroutine();
 
-    }*/
+        _lastOffset = offset;
+
+        _coroutine = ChangeRotation(quaternion);
+        StartCoroutine(_coroutine);
+    }
 
     private IEnumerator ChangeRotation(Quaternion rotation)
     {
         _coroutineIsActive = true;
+        float delayGradualIncrease = 0;
 
         while (transform.rotation != rotation)
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, _zoomSpeed * Time.deltaTime * 2f);       // Magical number
+            if (delayGradualIncrease < _zoomSpeed)
+                delayGradualIncrease += Time.deltaTime * _accelerationZoomCoefficient;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, delayGradualIncrease * Time.deltaTime);
             yield return null;
         }
 
